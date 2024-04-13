659cec23e42ddf4f924dcbe4a19644819d0d68cc
Joshwa <joshwa@Joshwas-MacBook-Air.local>
2024-04-13 16:27:23 +0530

    task 4 completed.

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Shang Chi',
+        des: 'Shang-Chi, a martial arts master, must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.',
+        image: 'images/shang chi top.webp'
+    },
+    {
+        name: 'Frozen II',
+        des: 'Why was Elsa born with magical powers? Together with Anna, Kristoff, Olaf, and Sven, she sets out on a dangerous but remarkable journey to find the answer.',
+        image: 'images/frozen top.webp'
+    },
+    {
+        name: 'Monster',
+        des: 'Trying to cope with the hardships, Bhamini and Anil`s life turns catastrophic as Lucky Singh invades. Secrets unveil when this uninvited guest starts his game.',
+        image: 'images/monster top.webp'
+    },
+    {
+        name: 'Label',
+        des: 'An attempt for justice gets squashed when forced to take an unjust path. Will an aspiring judge stand his moral ground despite the identity forced upon him?',
+        image: 'images/label top.webp'
+    },
+    {
+        name: 'Anbarivu',
+        des: 'Stoic and kind-hearted Arivu decides to switch places with his street-smart rowdy twin, Anbu, to unite their estranged families. And a frenzy begins!',
+        image: 'images/anbarivu top.webp'
+    }
+]
